75de6bf1812ff673ae9f5a082c12803b135c1d7b
prernadave <prernadave3914@gmail.com>
2023-01-20 23:43:58 +0530

    day-4

---
+++ backenddatabase/connection.js
@@ -0,0 +1,33 @@
+const mongoose = require("mongoose")
+const connection = mongoose.connect("mongodb+srv://prerna_dave:prernadave@cluster0.1el1sjr.mongodb.net/monamour?retryWrites=true&w=majority")
+
+const productSchema = mongoose.Schema({
+    
+        imageURL: { type: String, required: true },
+        brand: { type: String, required: true },
+        product: { type: String, required: true },
+        gender: { type: String, required: true },
+        category: { type: String, required: true },
+        price: { type: Number, required: true },
+        strikedOffPrice: { type: Number, required: false },
+      },
+      {
+        versionKey: false,
+        timestamps: true,
+      }
+    );
+
+
+
+const ProductModel = mongoose.model("product",productSchema);
+
+
+const usermodel = mongoose.Schema({
+  
+})
+
+
+module.exports={
+    connection,
+    ProductModel
+}
